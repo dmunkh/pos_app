8a57aa87fa8c5c35f4cf3342c25038b59745f3c9
dmunkh <dmunkh84@gmail.com>
2024-04-14 00:43:54 +0800

    dmunkh

--- src/pages/Friends/Friends.js
+++ src/pages/Friends/Friends.js
@@ -6,44 +6,63 @@ import { Column } from "primereact/column";
 import dayjs from "dayjs";
 import customParseFormat from "dayjs/plugin/customParseFormat";
 import { DatePicker, Space } from "antd";
+import axios from "axios";
 
 const Friends = () => {
   const usenavigate = useNavigate();
   const [customerlist, listupdate] = useState(null);
   const currentDate = dayjs();
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
 
   const dateFormat = "YYYY/MM/DD";
   const weekFormat = "MM/DD";
   const monthFormat = "YYYY/MM";
 
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://9xz5rjl8ej.execute-api.us-east-1.amazonaws.com/production/users"
+        );
+        console.log(response.data);
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   console.log(sessionStorage.getItem("username"));
-  const data = [
-    {
-      name: "Dune",
-      author: "Frank Herbert",
-      year: "1965",
-    },
-    {
-      name: "Ender's Game",
-      author: "Orson Scott Card",
-      year: "1985",
-    },
-    {
-      name: "The Hitchhiker's Guide to the Galaxy",
-      author: "Douglas Adams",
-      year: "1979",
-    },
-    {
-      name: "1984",
-      author: "George Orwell",
-      year: "1949",
-    },
-    {
-      name: "Brave New World",
-      author: "Aldous Huxley",
-      year: "1932",
-    },
-  ];
+  // const data = [
+  //   {
+  //     name: "Dune",
+  //     author: "Frank Herbert",
+  //     year: "1965",
+  //   },
+  //   {
+  //     name: "Ender's Game",
+  //     author: "Orson Scott Card",
+  //     year: "1985",
+  //   },
+  //   {
+  //     name: "The Hitchhiker's Guide to the Galaxy",
+  //     author: "Douglas Adams",
+  //     year: "1979",
+  //   },
+  //   {
+  //     name: "1984",
+  //     author: "George Orwell",
+  //     year: "1949",
+  //   },
+  //   {
+  //     name: "Brave New World",
+  //     author: "Aldous Huxley",
+  //     year: "1932",
+  //   },
+  // ];
   return (
     <>
       <h1 className="text-3xl font-bold underline text-red-600">
@@ -51,10 +70,10 @@ const Friends = () => {
       </h1>
       <DatePicker defaultValue={currentDate} format={dateFormat} />
       <DataTable value={data} tableStyle={{ minWidth: "50rem" }}>
-        <Column field="code" header="Code"></Column>
-        <Column field="name" header="Name"></Column>
-        <Column field="author" author="author"></Column>
-        <Column field="year" header="year"></Column>
+        <Column field="shortname" header="Code"></Column>
+        <Column field="user_info" header="Name"></Column>
+        <Column field="userid" author="author"></Column>
+        <Column field="username" header="year"></Column>
       </DataTable>
     </>
   );
--- src/pages/Login/Login.js
+++ src/pages/Login/Login.js
@@ -1,14 +1,49 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import styles from "./Login.module.css";
 import Toast from "../../UI/Toast/Toast";
 import useBearStore from "../../state/state";
 import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 
 const Login = (props) => {
+  const [customerlist, listupdate] = useState(null);
+  const [data, setData] = useState(null);
+  const [error, setError] = useState(null);
   const [formData, setFormData] = useState({
     username: "",
     password: "",
   });
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(
+          "https://9xz5rjl8ej.execute-api.us-east-1.amazonaws.com/production/users"
+        );
+        console.log(response.data);
+        setData(response.data);
+      } catch (error) {
+        setError(error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
+  // useEffect(() => {
+  //   sessionStorage.clear();
+
+  //   fetch(
+  //     "https://9xz5rjl8ej.execute-api.us-east-1.amazonaws.com/production/users"
+  //   )
+  //     .then((res) => {
+  //       console.log(res.json(), res);
+  //       return res.json();
+  //     })
+  //     .catch((err) => {
+  //       console.log(err.messsage);
+  //     });
+  // }, []);
 
   // const [message, setMessage] = useState("");
   const [toast, setToast] = useState({
@@ -87,17 +122,17 @@ const Login = (props) => {
             onChange={onChange}
             value={formData.username}
             type={"text"}
-            id='username'
-            name='username'
-            placeholder='username or email'
+            id="username"
+            name="username"
+            placeholder="username or email"
           />
           <input
             onChange={onChange}
             value={formData.password}
             type={"password"}
-            id='password'
-            name='password'
-            placeholder='password'
+            id="password"
+            name="password"
+            placeholder="password"
           />
           <button>submit</button>
           {/* <div className={styles["err-msg-div"]}>{message}</div> */}
